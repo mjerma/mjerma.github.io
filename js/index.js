36b7a72fc099fe085d98156e9828fe90e9d03102
mjerma <mihael.majer283@gmail.com>
2023-03-01 13:51:33 +0100

    Price calculator

--- js/index.js
+++ js/index.js
@@ -1,17 +1,1 @@
-$(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
-      $('nav').addClass('shrink');
-    } else {
-      $('nav').removeClass('shrink');
-    }
-  });
 
-  lightbox.option({
-    'resizeDuration': 200,
-    'wrapAround': true
-  })
-
-
-    function redirect() {
-        window.location.href= "index.html";  
-      }
